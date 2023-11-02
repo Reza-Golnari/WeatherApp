2a5bbd6ad719cf4c285016e932739efbb288c72c
Reza <i.am.reza.programmer@gmail.com>
2023-11-02 12:14:46 +0330

    Create base of app

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="w-100">
+    <div class="input-box w-100">
+      <v-sheet width="300">
+        <v-form class="w-100" @submit.prevent>
+          <v-text-field
+            v-model="cityName"
+            label="City name"
+            class="w-100"
+          ></v-text-field>
+          <v-btn type="submit" block class="mt-2">Go</v-btn>
+        </v-form>
+      </v-sheet>
+    </div>
+  </div>
+</template>
+
+<script setup>
+let cityName = ref("");
+</script>
+
+<style scoped></style>
